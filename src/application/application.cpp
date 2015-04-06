27d23c4c02b7144aeb813d0e874a8869b36002b2
Jacob M. Slone <jslone1993@gmail.com>
2015-04-06 01:42:31 -0400

    fixing more build stuff

--- src/application/application.cpp
+++ src/application/application.cpp
@@ -1,9 +1,12 @@
+#include <iostream>
+#include "application.h"
+
 void Application::init() {
-	
+	std::cout << "hi" << std::endl;
 }
 
 void Application::uninit() {
-
+	std::cout << "bye" << std::endl;
 }
 
 int main(int argc, char **argv) {
