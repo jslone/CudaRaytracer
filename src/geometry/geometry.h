b31202a955976e6b63eb14a66fd46d71d38591c6
Jacob M. Slone <jslone1993@gmail.com>
2015-04-17 15:14:03 -0400

    mesh and faces starter

--- src/geometry/geometry.h
+++ src/geometry/geometry.h
@@ -1,8 +1,34 @@
 #ifndef _GEOMETRY_H_
 #define _GEOMETRY_H_
 
+#include "math/math.h"
+
 namespace acr {
 
+  struct Vertex {
+    math::vec3 position;
+    math::vec3 normal;
+    math::vec3 color;
+  };
+
+  struct Face {
+    uint32_t indices[3];
+  };
+
+
+  class Mesh {
+    public:
+      Mesh(float *positions, float *normals, float *colors, uint32_t *indices, uint32_t numVertices, uint32_t numFaces);
+      ~Mesh();
+    private:
+      Vertex   *vertices;
+      Face     *faces;
+      uint32_t numVertices;
+      uint32_t numFaces;
+
+      char *data;
+  };
+  
 } // namespace acr
 
 #endif //_GEOMETRY_H_
